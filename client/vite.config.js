2d0d32299089579ecb9628da8fdd6c122e4c4893
MadgulaVamshi06 <vamshimadgula@gmail.com>
2025-06-15 22:50:42 +0530

    updated tailwind css

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import vitePluginImp from 'vite-plugin-imp';
+
 export default defineConfig({
   plugins: [
-    tailwindcss(),
+    react(),
+    vitePluginImp({
+      libList: [
+        {
+          libName: 'antd',
+          style: (name) => `antd/es/${name}/style/index`,
+        },
+      ],
+    }),
   ],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://jumia-clone-backend.onrender.com/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
